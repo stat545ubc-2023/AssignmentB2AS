427b990e31bab3d36c694da55c6528e7cfae3e0b
andrasszeitz <andras.szeitz@ubc.ca>
2023-11-09 17:17:17 -0800

    committ

--- tests/testthat/test-testthat.R
+++ tests/testthat/test-testthat.R
@@ -1,4 +1,4 @@
-library(ggplot2)
+ibrary(ggplot2)
 library(datateachr)
 
 test_that("Test 1: Function creates a ggplot with a custom x-axis name", {
@@ -7,5 +7,19 @@ test_that("Test 1: Function creates a ggplot with a custom x-axis name", {
 })
 
 test_that("Test 2: Function has categorical fill values", {
-  expect_error(scatter_plot( data=cancer_sample, x = cancer_sample$perimeter_mean, y = cancer_sample$texture_mean))
-  })
+  expect_error(scatter_plot(data=cancer_sample, x = cancer_sample$perimeter_mean, y = cancer_sample$texture_mean))
+})
+
+test_that("Test 3: Function uses the right dataset", {
+  scatter_plot(x = cancer_sample$perimeter_mean, y = cancer_sample$texture_mean)
+  expect_true(TRUE, "Test passed.")
+})
+
+test_that(""Test 4:To see if the function requires a dataset and variables, rather than a row of numbers", {
+  # Checking if the arguments are data frames and character strings
+  expect_true(is.data.frame(cancer_sample) && is.character("perimeter_mean") && is.character("texture_mean"), "Test passed.")
+})
+
+test_that("Test 5: Function can only use numerical dataset", {
+  expect_error(easyplot(data=cancer_sample, x = cancer_sample$perimeter_mean, y = cancer_sample$texture_mean))
+})

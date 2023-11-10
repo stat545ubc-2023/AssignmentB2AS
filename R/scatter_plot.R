f7b2d83fea287cab3a48b2350c96d661283bf791
andrasszeitz <andras.szeitz@ubc.ca>
2023-11-09 18:11:50 -0800

    committ

--- R/scatter_plot.R
+++ R/scatter_plot.R
@@ -1,31 +1,25 @@
-#' scatter_plot
-#' @description The AssignmentB2AS package generates a scatter plot with ggplot2. Using selected data sets, this package offers a quick way to assess if there is a relationship between independent (x) and dependent (y) variables with the visualization options of bullet size 1 and purple color.
+#' Generate a scatter plot.
 #'
-#' @param x Data for the x-axis.
-#' @param y Data for the y-axis.
-#' @param x_label Label for the x-axis.
-#' @param y_label Label for the y-axis.
-#' @param title Title of the plot. 
-#' @return A scatter plot
+#' This function generates a scatter plot using ggplot2.
+#'
+#' @param data The input dataset.
+#' @param x A continuous numerical variable in the dataset, it is one of the variable being investigated.
+#' @param y A continuous numerical variable in the dataset, it is one of the variable being investigated.
+#' @param x_label This is the name of the x-axis given by the user who uses this function.
+#' @param y_label This is the name of the y-axis given by the user who uses this function.
+#' @param title The title of the scatter plot.
+#'
+#' @return A ggplot object representing the scatter plot.
+#'
 #' @examples
-#' Load the dataset
-#' data(cancer_sample)
-#' Example code here
-#' scatter_plot(x = datateachr::cancer_sample$perimeter_mean, y = datateachr::cancer_sample$texture_mean)
+#' data <- datateachr::cancer_sample
+#' scatter_plot(data = data, x = data$perimeter_mean, y = data$texture_mean, x_label = "Perimeter", y_label = "Texture", title = "Scatter Plot Example")
+#'
+#' @import ggplot2
+#' @importFrom ggplot2 ggplot aes geom_point labs
 #' @export
-
-library(datateachr)
-library(ggplot2)
-scatter_plot <- function(x, y, x_label = "X-axis", y_label = "Y-axis", title = "Scatter Plot") {
-  plot_data <- data.frame(x = x, y = y)
-  
-  # Suppressing warnings while creating the plot
-  suppressWarnings({
-    scatter_plot <- ggplot(plot_data, aes(x = x, y = y)) +
-      geom_point(size = 1, color = "purple") +
-      labs(x = x_label, y = y_label, title = title)
-  })
-  
-  # Printing the scatter plot
-  print(scatter_plot)
+scatter_plot <- function(data, x, y, x_label = "X-axis", y_label = "Y-axis", title = "Scatter Plot") {
+  ggplot(data, aes(x = {{x}}, y = {{y}})) +
+    geom_point() +
+    labs(x = x_label, y = y_label, title = title)
 }

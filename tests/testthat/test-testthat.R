df7e6393b2c262d49184668bdfe3b6f9dadc8507
andrasszeitz <andras.szeitz@ubc.ca>
2023-11-09 16:47:26 -0800

    committ

--- tests/testthat/test-testthat.R
+++ tests/testthat/test-testthat.R
@@ -6,14 +6,6 @@ test_that("Test 1: Function creates a ggplot with a custom x-axis name", {
   expect_is(expected_val, "ggplot")
 })
 
-test_that("Test 2: Function haves continuous numerical x values", {
-  expect_error(scatter_plot(cancer_sample, x = cancer_sample$perimeter_mean, y = cancer_sample$texture_mean))
-})
-
-test_that("Test 3: Function takes four input variable", {
-  expect_error(scatter_plot(cancer_sample, x = cancer_sample$perimeter_mean, y = cancer_sample$texture_mean))
-})
-
 test_that("Test 4: Function has categorical fill values", {
   expect_error(scatter_plot( data=cancer_sample, x = cancer_sample$perimeter_mean, y = cancer_sample$texture_mean))
   })
